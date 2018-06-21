39fddad751cdd27b049b46063e38d732e0848276
avnicolaevich <avnicolaevich@gmail.com>
2018-06-22 02:31:40 +0300

    add slider to index page

--- app/js/init.js
+++ app/js/init.js
@@ -13,4 +13,41 @@
 
 	// Init jquery form styler
 	$('.select-styler, .input-file-styler').styler();
+
+	// Init trend products carousel
+	$(".trend-products-carousel").slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		// infinite: false,
+  		speed: 300,
+		dots: true,
+		autoplay: false,
+		autoplaySpeed: 5000,
+		arrows: false,
+		responsive: [
+			{
+			  	breakpoint: 1024,
+			  	settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+			  	}
+			},
+			{
+			  	breakpoint: 600,
+			  	settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+			 	}
+			},
+			{
+			  	breakpoint: 480,
+			  	settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+			  	}
+			}
+		]
+	});
 })(jQuery);
